a288262fc6f5eb097f1de52402f4579fadfaf399
Tarun <tarunjaluthria.tj@gmail.com>
2026-07-08 13:57:10 +0530

    set date and time

--- Assets/Interactivity/script.js
+++ Assets/Interactivity/script.js
@@ -5,6 +5,31 @@ import pomodoro from './pomodoro.js';
 import todo from './todo.js';
 import motivation from './motivation.js';
 
+const taarik = document.querySelector('.date-time p');
+const samay = document.querySelector('.date-time h4');
+
+function currentDate(){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    taarik.textContent = day.toString().padStart(2,'0') + '-' + month.toString().padStart(2,"0") + '-' + year;
+}
+
+setInterval(()=>{
+    let date = new Date();
+    const hours24 = date.getHours();
+    let hours = date.getHours() % 12 || 12;
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    const ampm = hours24 >= 12 ? 'PM' : 'AM';
+    samay.textContent = hours.toString().padStart(2,"0") + ':' + minutes.toString().padStart(2,"0") + ':' + seconds.toString().padStart(2,"0") + " " + ampm;
+},1000);
+
+currentDate();
+
+
+
 goback();
 getTheme();
 open();
